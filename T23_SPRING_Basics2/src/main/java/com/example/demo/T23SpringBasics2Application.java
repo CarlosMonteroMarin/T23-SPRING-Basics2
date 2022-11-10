e3e7a5278600c4a157733808df2e517abe8775ce
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-10 11:04:08 +0100

    Commit estructura proyecto

---
+++ T23_SPRING_Basics2/src/main/java/com/example/demo/T23SpringBasics2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T23SpringBasics2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T23SpringBasics2Application.class, args);
+	}
+
+}
